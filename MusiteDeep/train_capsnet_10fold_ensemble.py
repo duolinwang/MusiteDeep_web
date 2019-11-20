56c340bcfc8b7ad4797f9818c85ece681a99bd9f
duolinwang <wangdu@missouri.edu>
2019-11-19 22:40:47 -0600

    Add files via upload

--- MusiteDeep/train_capsnet_10fold_ensemble.py
+++ MusiteDeep/train_capsnet_10fold_ensemble.py
@@ -65,17 +65,17 @@ def calculate_avg_weights(inputweights,model_arch):
 if __name__ == "__main__":
     parser=argparse.ArgumentParser()
     parser.add_argument('-input',  dest='inputfile', type=str, help='training data in fasta format. Sites followed by "#" are positive sites for a specific PTM prediction.', required=True)
-    parser.add_argument('-output-prefix',  dest='outputprefix', type=str, help='prefix of the output files (model and parameter files).', required=True)
+    parser.add_argument('-output',  dest='outputfolder', type=str, help='folder for the output models (model and parameter files).', required=True)
     parser.add_argument('-residue-types',  dest='residues', type=str, help='Residue types that this model focus on. For multiple residues, seperate each with \',\'. \n\
     Note: all the residues specified by this parameter will be trained in one model.', required=True)
-    parser.add_argument('-nclass',  dest='nclass', type=int, help='number of classifiers to be trained for one time. [Default:5]', required=False, default=5)
+    parser.add_argument('-nclass',  dest='nclass', type=int, help='number of classifiers to be trained for one time. [Default:5]', required=False, default=1)
     parser.add_argument('-window',  dest='window', type=int, help='window size: the number of amino acid of the left part or right part adjacent to a potential PTM site. 2*\'windo size\'+1 amino acid will be extracted for one protential fragment. [Default:16]', required=False, default=16)
     parser.add_argument('-maxneg',  dest='maxneg', type=int, help='maximum iterations for each classifier which controls the maximum copy number of the negative data which has the same size with the positive data. [Default: 30]', required=False, default=30)
     parser.add_argument('-nb_epoch',  dest='nb_epoch', type=int, help='number of epoches for one bootstrap step. It is invalidate, if earlystop is set.', required=False, default=None)
     parser.add_argument('-earlystop',  dest='earlystop', type=int, help='after the \'earlystop\' number of epochs with no improvement the training will be stopped for one bootstrap step. [Default: 20]', required=False, default=20)
     parser.add_argument('-inputweights',  dest='inputweights', type=str, help='Initial weights saved in a HDF5 file.', required=False, default=None)
-    parser.add_argument('-checkpointweights',  dest='checkpointweights', type=str, help='prefix of the intermediate checkpoint files.', required=False, default=None)
-    parser.add_argument('-save_best_only', dest='save_best_only',action='store_true',help="save the best models in each iteration")
+    parser.add_argument('-checkpointweights',  dest='checkpointweights', type=str, help='folder for the intermediate checkpoint files.', required=True, default=None)
+    parser.add_argument('-save_best_only', dest='save_best_only',action='store_true',help="save the best models in each iteration if -load_average_weight is set, this must be set.")
     parser.add_argument('-load_average_weight',dest='load_average_weight', action="store_true",help="whether load the average weights of best and last model, if seted the save_best_only will be changed to True")
     parser.add_argument('-balance_val',dest='balance_val', action="store_true",help="use the balanced validation set to monitor the training process")
     args = parser.parse_args()
@@ -85,11 +85,11 @@ if __name__ == "__main__":
            print("Because load_average_weight has been set, changing save_best_only to "+str(args.save_best_only))
     
     residues=args.residues.split(",")
-    ensure_dir(args.outputprefix)
+    ensure_dir(args.outputfolder)
     ensure_dir(args.checkpointweights)
     
-    outputmodel = args.outputprefix+str("_HDF5model")
-    outputparameter = args.outputprefix+str("_parameters")
+    outputmodel = args.outputfolder+str("model_HDF5model")
+    outputparameter = args.outputfolder+str("model_parameters")
     codemode=0 #coding method
     model='nogradientstop' #use this model
     nb_classes=2 # binary classification
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     allfrag,folds=preprocess_data(args.inputfile,args.window,residues)
     for i,(train_indices,val_indices) in enumerate(folds):
         for bt in range(args.nclass):
-            checkpointoutput=args.checkpointweights+"_fold"+str(i)+"_nclass"+str(bt)
+            checkpointoutput=args.checkpointweights+"weights_fold"+str(i)+"_nclass"+str(bt)
             models=bootStrapping_allneg_continue_keras2(allfrag[train_indices],allfrag[val_indices],
                                                       srate=1,nb_epoch1=1,nb_epoch2=args.nb_epoch,earlystop=args.earlystop,maxneg=args.maxneg,
                                                       outputweights=checkpointoutput,
