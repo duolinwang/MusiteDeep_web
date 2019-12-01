d66073bb2121bf6f10dd3bbc18a420831755900d
duolinwang <wangdu@missouri.edu>
2019-11-30 19:42:16 -0600

    Add files via upload

--- mongoblast/user_query.py
+++ mongoblast/user_query.py
@@ -5,33 +5,49 @@
 import os
 import argparse
 import subprocess
+ptm_values={
+"Phosphotyrosine":"\"Phosphotyrosine\"",
+"Phosphoserine":"Phosphoserine",
+"Phosphothreonine":"Phosphothreonine",
+"N-linked_glycosylation":"\"N-linked (GlcNAc) asparagine\"",
+"O-linked_glycosylation":"\"O-linked (GlcNAc) serine_O-linked (GlcNAc) threonine\"",
+"Ubiquitination":"\"Glycyl lysine isopeptide (Lys-Gly)(interchain with G-Cter in ubiquitin)\"",
+"SUMOylation":"\"Glycyl lysine isopeptide (Lys-Gly)(interchain with G-Cter in SUMO)\"",
+"N6-acetyllysine": "\"N6-acetyllysine\"",
+"Methylarginine":"\"Omega-N-methylarginine_Dimethylated arginine_Symmetric dimethylarginine_Asymmetric dimethylarginine\"",
+"Methyllysine": "\"N6-methyllysine_N6,N6-dimethyllysine_N6,N6,N6-trimethyllysine\"",
+"Pyrrolidone_carboxylic_acid":"\"Pyrrolidone carboxylic acid\"",
+"S-palmitoyl_cysteine":"\"S-palmitoyl cysteine\"",
+"Hydroxyproline":"3-hydroxyproline_4-hydroxyproline",
+"Hydroxylysine":"4,5-dihydroxylysine_3,4-dihydroxylysine_5-hydroxylysine"
+}
 
 def main():
     path = os.path.dirname(os.path.realpath(__file__))
     parser = argparse.ArgumentParser()
     parser.add_argument('-query', help="query sequence in FASTA format (currently, only one sequence is allowed).",required=True)
-    parser.add_argument('-l', default=path,help="local filepath, where to put the intermediate blast results (default: current folder)",required=False)
-    parser.add_argument('-ptms', default='Phosphoserine_Phosphothreonine', help="PTMs to be annotated (default: 'Phosphoserine_Phosphothreonine'). For multiple ones, use \"_\" to seperate them. For example: ptm1_ptm2_ptm3.\n\
-                       Currently, we supported\n: Phosphoserine, Phosphothreonine, Phosphotyrosine,\
-                       N-linked (GlcNAc) asparagine,\
-                       O-linked (GlcNAc) serine_O-linked (GlcNAc) threonine,\
-                       Glycyl lysine isopeptide (Lys-Gly)(interchain with G-Cter in ubiquitin),\
-                       Glycyl lysine isopeptide (Lys-Gly)(interchain with G-Cter in SUMO),\
+    parser.add_argument('-ptms', default='Phosphoserine;Phosphothreonine', help="PTMs to be annotated (default: 'Phosphoserine;Phosphothreonine'). For multiple ones, use \";\" to seperate them. For example: ptm1;ptm2;ptm3.\n\
+                       Currently, we supported\n: \
+                       Phosphoserine, Phosphothreonine, Phosphotyrosine,\
+                       N-linked_glycosylation,\
+                       O-linked_glycosylation,\
+                       Ubiquitination,\
+                       SUMOylation,\
                        N6-acetyllysine,\
-                       Omega-N-methylarginine_ Dimethylated arginine_Symmetric dimethylarginine_Asymmetric dimethylarginine,\
-                       N6-methyllysine_N6,N6-dimethyllysine_N6,N6,N6-trimethyllysine,\
-                       Pyrrolidone carboxylic acid,\
-                       S-palmitoyl cysteine,\
-                       3-hydroxyproline_4-hydroxyproline,\
-                       4,5-dihydroxylysine_3,4-dihydroxylysine_5-hydroxylysine"
+                       Methylarginine,\
+                       Methyllysine,\
+                       Pyrrolidone_carboxylic_acid,\
+                       S-palmitoyl_cysteine,\
+                       Hydroxyproline,\
+                       Hydroxylysine"
                        )
     
     parser.add_argument('-evalue', default=1e-5, help="blastp evalue (default:1e-5)",required=False)
     parser.add_argument('-max_target_seqs', default=50, help="Maximum number of target sequences to be returned (default:50)",required=False)
     parser.add_argument('-o', default=path+'/display',help="output folder name (default: ./display)",required=False)
     args = parser.parse_args()
-    filepath = args.l
-    ptms = args.ptms
+    ptms = "_".join([ptm_values[x] for x in args.ptms.split(";")])
+    print("selected PTM:"+ptms+"\n")
     evalue = args.evalue
     out_folder = args.o
     max_target_seqs = args.max_target_seqs
@@ -47,9 +63,9 @@ def main():
         print("More than one sequences are provided, but we currently accept only one query sequence!")
         exit(1)
         
-    step_1 = 'blastp -query '+query+' -db mydb -evalue '+str(evalue)+' -max_target_seqs '+str(max_target_seqs)+' -outfmt 11 -out '+filepath+'/format11.asn'
-    step_2 = 'python3 codes/blast_parse.py -l '+filepath+'/format11.asn'+' -ptms '+ptms+' -o '+out_folder
-    step_3 = 'python3 codes/merge_results.py -queryId \"'+queryId+'\" -blastFolder '+out_folder+' -ptms '+ptms
+    step_1 = 'blastp -query '+query+' -db mydb -evalue '+str(evalue)+' -max_target_seqs '+str(max_target_seqs)+' -outfmt 11 -out '+out_folder+'/format11.asn'
+    step_2 = 'python3 '+path+'/codes/blast_parse.py -l '+out_folder+'/format11.asn'+' -ptms '+ptms+' -o '+out_folder
+    step_3 = 'python3 '+path+'/codes/merge_results.py -queryId \"'+queryId+'\" -blastFolder '+out_folder+' -ptms '+ptms
     subprocess.call([step_1],shell=True)
     subprocess.call([step_2],shell=True)
     subprocess.call([step_3],shell=True)
@@ -58,3 +74,5 @@ def main():
 
 if __name__== "__main__":
 	main()
+
+#python3 user_query.py -query example_query_seqs.fasta -ptms "Phosphoserine_Phosphothreonine_Phosphotyrosine"  -o blastoutput
